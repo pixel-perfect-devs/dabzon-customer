f7e85b5c5b3ab2cc6aa001febf978828d6f4dca9
Deepak Mishra <deepmishradm12357@gmail.com>
2023-04-12 20:25:00 +0530

    google-auth implemented

--- src/components/Auth/LoginComponent/index.jsx
+++ src/components/Auth/LoginComponent/index.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 import Link from 'next/link'
 import login_bg from '../../../../public/auth/login_bg.svg'
 import { useState } from 'react'
+import { setCookie, getCookie } from '@/cookie'
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 const Index = () => {
+  const {data : session} = useSession();
   const [userData, setUserData] = useState({ email: '', password: '' })
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -16,8 +19,20 @@ const Index = () => {
       body: JSON.stringify(userData),
     });
     const data = await res.json();
-    console.log(data)
+    if(data.msg === "wrong email or password"){
+      alert("wrong email or password")
+    }
+    else{
+      setCookie("userSession", data.body.email,1);
+    }
   }
+ 
+  const handleGoogleSignin = async() => {
+    await signIn("google");
+    setCookie("userSession", session.user.email,1);
+
+  }
+
   return (
     <section className="flex items-center justify-evenly min-h-screen bg-gray-100 flex-wrap py-16">
       <Image width={445} height={445} src={login_bg} className="m-2 hidden md:block" alt="Sample image" />
@@ -75,13 +90,13 @@ const Index = () => {
             Login
           </button>
           <p className="opacity-60 text-[0.8rem] my-[1rem]">OR</p>
-          <button className="signupwithgoogle bg-[#10b981] cursor-pointer rounded-full p-2 text-white flex items-center gap-3">
+          <button onClick={() => handleGoogleSignin()} className="signupwithgoogle bg-[#10b981] cursor-pointer rounded-full p-2 text-white flex items-center gap-3">
             <span className="auth__icon ">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-google" viewBox="0 0 16 16">
                 <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
               </svg>
             </span>
-            <p className="">SignUp with Google</p>
+            <p className="">Signin with Google</p>
           </button>
         </div>
         <p className="opacity-60 text-[0.8rem] my-12 flex gap-3">
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,14 @@
 import '@/styles/globals.css'
 import { store } from '../reduxStore/store'
 import { Provider } from 'react-redux'
+import {SessionProvider} from 'next-auth/react'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <Component {...pageProps} />
-    </Provider>
+    <SessionProvider>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </SessionProvider>
   )
 }
