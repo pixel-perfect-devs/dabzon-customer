7c3301b50154159f31ec8476dd661a94a43e412d
Saurabh Sen <sensaurabh1212@gmail.com>
2023-04-14 16:53:34 +0530

    something by saurabh

--- src/pages/user/profile.jsx
+++ src/pages/user/profile.jsx
@@ -4,10 +4,14 @@ import FooterComponents from '../../components/FooterComponents/index'
 
 const Profile = () => {
   return (
-    <div className='profile'>
+    <div className='profile bg-blue-50'>
       <NavBar />
-      {/* my profile customer */}
-      <h1>Profile</h1>
+      <div className='profile__container max-w-7xl mx-auto px-[4vw] my-3'>
+        <div className="profile__left">
+          {/* copy paste your navbar here */}
+        </div>
+        <div className="profile__right"></div>
+      </div>
       <div className="footer hidden sm:block">
         <FooterComponents />
       </div>
