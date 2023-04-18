2526940e1da416f3c8b20591fd72a240ea25f822
Saurabh Sen <sensaurabh1212@gmail.com>
2023-04-18 21:29:27 +0530

    edit address

--- src/components/Modals/Address/index.jsx
+++ src/components/Modals/Address/index.jsx
@@ -1,71 +1,62 @@
-import React,{useState,useEffect} from "react";
+import React, { useState } from "react";
 
-const index = ({ setModal }) => {
-    const [getData, setGetData] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    useEffect(() => {
-      async function fetchData() {
-        try {
-          const response = await fetch('/api/address/get');
-          const data = await response.json();
-          console.log(data.allData);
-          setGetData(data.allData);
-        } catch (error) {
-          console.error(error);
-        } finally {
-          setIsLoading(false);
-        //   console.log(faqData[0]);
-        }
-      }
-  
-      fetchData();
-    }, []);
+const index = ({ address, setShowAddressModal }) => {
 
-    const [postData, setPostData] = useState({
-        name:"",
-        number:"",
-        email:"",
-        pincode:"",
-        address:"",
-        city:"",
-    })
-    const onSubmit = async (e) => {
-      e.preventDefault();
-      // console.log(postData)
-      if(postData.city === '' || postData.address === '' || postData.email === '' || postData.name === '' || postData.number === '' || postData.pincode === ''){ 
-        alert('choose city');
-        return;
-      }
-        const res = await fetch(`/api/address/post`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json', 
-            },
-            body: JSON.stringify(postData),
-          });
-          const data = await res.json();
-          if (res.status === 200) {
-            alert("Product Created");
-          }
-          console.log(data);
+  const [postData, setPostData] = useState({
+    name: "",
+    number: "",
+    email: "",
+    pincode: "",
+    address: "",
+    city: "",
+  })
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    // console.log(postData)
+    if (postData.city === '' || postData.address === '' || postData.email === '' || postData.name === '' || postData.number === '' || postData.pincode === '') {
+      alert('choose city');
+      return;
     }
+    const res = await fetch(`/api/address/post`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(postData),
+    });
+    const data = await res.json();
+    if (res.status === 200) {
+      alert("Product Created");
+    }
+    console.log(data);
+  }
+
+  const cancelEdit = (e) => {
+    e.preventDefault();
+    setShowAddressModal({
+      showModal: false,
+      address: null
+    })
+  }
+
   return (
-    <div className="Modal_container flex items-center h-[100vh] py-8 z-">
+    <div className="Modal_container flex items-center h-full py-8">
       <form onSubmit={(e) => onSubmit(e)} className="flex flex-col border-2 border-dabgreen justify-center items-center space-y-4 w-[40rem] h-[38rem] p-6 mx-auto bg-gray-100 rounded-md shadow-md">
         <div className="customer__name flex flex-col">
           <label className="text-gray-900 " htmlFor="name">
             Your Name
           </label>
           <input
-              onChange={(e) =>
-                setPostData((prev) => (prev = { ...prev, name: e.target.value }))
-              }
+            onChange={(e) =>
+              setPostData((prev) => (prev = { ...prev, name: e.target.value }))
+            }
             className="border-gray-300 border rounded-md p-2 outline-0 bg-transparent w-[300px] "
             type="text"
             id="name"
             name="name"
             placeholder="Enter your name"
             required
+            value={address.name}
           />
         </div>
         <div className="customer__number flex flex-col">
@@ -73,15 +64,16 @@ const index = ({ setModal }) => {
             Mobile no.
           </label>
           <input
-              onChange={(e) =>
-                setPostData((prev) => (prev = { ...prev, number: e.target.value }))
-              }
+            onChange={(e) =>
+              setPostData((prev) => (prev = { ...prev, number: e.target.value }))
+            }
             className="border-gray-300 border rounded-md p-2 outline-0 bg-transparent w-[300px] "
             type="number"
             id="mobile"
             name="mobile"
             placeholder="Enter your Mobile no."
             required
+            value={address.number}
           />
         </div>
         <div className=" customer__email flex flex-col ">
@@ -89,15 +81,16 @@ const index = ({ setModal }) => {
             Your Email
           </label>
           <input
-              onChange={(e) =>
-                setPostData((prev) => (prev = { ...prev, email: e.target.value }))
-              }
+            onChange={(e) =>
+              setPostData((prev) => (prev = { ...prev, email: e.target.value }))
+            }
             className="border-gray-300 border rounded-md p-2 outline-0  bg-transparent w-[300px] "
             type="email"
             id="email"
             name="email"
             placeholder="Enter your email..."
             required
+            value={address.email}
           />
         </div>
 
@@ -106,15 +99,16 @@ const index = ({ setModal }) => {
             Pin Code
           </label>
           <input
-              onChange={(e) =>
-                setPostData((prev) => (prev = { ...prev, pincode: e.target.value }))
-              }
+            onChange={(e) =>
+              setPostData((prev) => (prev = { ...prev, pincode: e.target.value }))
+            }
             className="border-gray-300 border rounded-md p-2 outline-0  bg-transparent w-[300px] "
             type="number"
             id="pincode"
             name="pincode"
             placeholder="Enter your pincode..."
             required
+            value={address.pincode}
           />
         </div>
 
@@ -123,38 +117,45 @@ const index = ({ setModal }) => {
             Full Address
           </label>
           <input
-              onChange={(e) =>
-                setPostData((prev) => (prev = { ...prev, address: e.target.value }))
-              }
+            onChange={(e) =>
+              setPostData((prev) => (prev = { ...prev, address: e.target.value }))
+            }
             className="border-gray-300 border rounded-md p-2 outline-0  bg-transparent w-[300px] "
             type="text"
             id="address"
             name="address"
             placeholder="Enter your address"
             required
+            value={address.address}
           />
         </div>
         <div className="flex flex-col">
 
-        <label className="text-gray-900 " htmlFor="dropdown">City</label>
-        <select onChange={(e) => setPostData((prev) => prev = {...prev, city: e.target.value})} className="border-gray-300 border rounded-md p-2 outline-0  bg-transparent w-[300px]" id="dropdown" required defaultValue={'not'}>
-          <option disabled value="not">Chooose your city</option>
-          <option value="Agra">Agra</option>
-          <option value="Delhi">Delhi</option>
-          <option value="Banglore">Banglore</option>
-        </select>
+          <label className="text-gray-900 " htmlFor="city">City</label>
+          <input
+            onChange={(e) =>
+              setPostData((prev) => (prev = { ...prev, city: e.target.value }))
+            }
+            className="border-gray-300 border rounded-md p-2 outline-0  bg-transparent w-[300px] "
+            type="text"
+            id="city"
+            name="city"
+            placeholder="Enter your city"
+            required
+            value={address.city}
+          />
 
         </div>
         <div className="flex justify-between w-72">
           <button
-          onClick={(e) => onSubmit(e)}
+            onClick={(e) => onSubmit(e)}
             className="flex justify-center items-center text-green-50 bg-dabgreen rounded-full w-24 h-9"
             type="submit"
           >
             Save
           </button>
           <button
-            onClick={() => setModal(false)}
+            onClick={(e) => cancelEdit(e)}
             className="flex justify-center items-center text-green-50 bg-dabgreen rounded-full w-24 h-9">
             Cancel
           </button>
--- src/pages/api/address/get.js
+++ src/pages/api/address/get.js
@@ -1,21 +1,18 @@
 import clientPromise from "../../../../backend/database/connect";
 export default async function handler(req, res) {
-    if(req.method === 'GET'){
+    if(req.method === 'POST'){
         try {
-            // console.log("hello g")
+            const { userId } = await req.body;
             const client = await clientPromise;
             const db = await client.db("dabzon");
             const collection = await db.collection("address");
-            const docs= await collection.findOne({ email: "yadavkapil2336@gmail.com" })
-            ;
-            console.log(docs);
-            if(docs.length !== 0){ //spelling
+            const docs= await collection.findOne({ email: userId });
+            if(docs){
                 return res.status(200).json({ allData:docs, msg:"successfully fetched all category data"});
             }
             else{
-                return res.status(501).json({ allData:"", msg:"no data found"});
+                return res.status(404).json({ allData:"", msg:"no data found"});
             }
-            // return res.status(200).json({ allData:"great job", msg:"successfully fetched all category data"});
           }
           catch (error) {
             console.log(error);
