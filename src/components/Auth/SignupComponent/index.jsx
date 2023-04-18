3ed0f743a1e030d8be9aee5900bf86586a1e0ca0
Deepak Mishra <deepmishradm12357@gmail.com>
2023-04-18 18:58:21 +0530

    signup with google

--- src/components/Auth/SignupComponent/index.jsx
+++ src/components/Auth/SignupComponent/index.jsx
@@ -2,14 +2,18 @@ import React, { useState } from "react";
 import Image from "next/image";
 import signup_bg from "../../../../public/auth/signup_bg.svg";
 import Link from "next/link";
+import { signIn, useSession } from "next-auth/react";
 
 const Index = () => {
+  const { data: session } = useSession();
   const [userData, setUserData] = useState({
     name: "",
     email: "",
     password: "",
-    confirmPassword: ""
+    confirmPassword: "",
+    googleAuth: false
   })
+  const [already, setAlready] = useState(false);
   const [message, setMessage] = useState("");
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -23,7 +27,7 @@ const Index = () => {
           if (userData.password === userData.confirmPassword) {
             // if entered data is correct
             setMessage("")
-            // delete userData.confirmPassword;
+            delete userData.confirmPassword;
             const res = await fetch(`${process.env.NEXT_PUBLIC_CUSTOMER_HOST}/api/user/signup`, {
               method: "POST",
               headers: {
@@ -52,6 +56,36 @@ const Index = () => {
       setMessage("Enter Name Correctly")
     }
   }
+
+  const handleGoogleSignup = (e) => {
+    e.preventDefault();
+    signIn("google");
+  }
+
+  const saveUserData = async () => {
+    // check the user exist already
+    const obj = { email: session.user.email, name: session.user.name, googleAuth: true, password: "" };
+    const res = await fetch(`${process.env.NEXT_PUBLIC_CUSTOMER_HOST}/api/user/signup`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(obj),
+    });
+    const data = await res.json();
+    if (data.msg === "user is already present") {
+      alert("you are already resgistered");
+    }
+    else {
+      setAlready(true);
+      alert("you are successfully registered");
+    }
+  }
+  
+  React.useEffect(() => {
+    if (session?.user?.email && !already) saveUserData();
+  }, [session]);
+
   return (
     <div className="flex items-center justify-evenly min-h-screen bg-gray-100 flex-wrap py-16">
       <Image className="m-2 hidden md:block" src={signup_bg} alt="Image is loading..."
@@ -145,7 +179,7 @@ const Index = () => {
             Create
           </button>
           <p className="opacity-60 text-[0.8rem] my-[1rem]">OR</p>
-          <button className="signupwithgoogle bg-dabgreen cursor-pointer rounded-full p-2 text-white flex items-center gap-3">
+          <button onClick={(e) => handleGoogleSignup(e)} className="signupwithgoogle bg-dabgreen cursor-pointer rounded-full p-2 text-white flex items-center gap-3">
             <span className="auth__icon ">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-google" viewBox="0 0 16 16">
                 <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
