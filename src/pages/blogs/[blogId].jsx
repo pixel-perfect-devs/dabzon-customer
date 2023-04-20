ba8c9d6d7320a9abc8a7035bcacb62b6aa966e2e
Saurabh Sen <sensaurabh1212@gmail.com>
2023-04-20 09:22:53 +0530

    blog page fixing

--- src/pages/blogs/[blogId].jsx
+++ src/pages/blogs/[blogId].jsx
@@ -4,35 +4,36 @@ import { useRouter } from 'next/router';
 import BlogDetailsPage from '../../components/BlogComponents/BlogDetailsPage/index'
 
 const BlogDetails = ({blog}) => {
-  const [item, setItem] = React.useState({});
-  React.useEffect(() => {
-    setItem(blog);
-  }, []);
+  // const [item, setItem] = React.useState({});
+  // React.useEffect(() => {
+  //   setItem(blog);
+  // }, []);
 
   return (
     <div className='BlogDetails '>
-      <BlogDetailsPage item={item} />
+      {/* <BlogDetailsPage item={item} /> */}
+      <h1>blogs details page</h1>
     </div>
   )
 }
 
 export default BlogDetails
 
-const client = createClient({
-  projectId: "q21v17fe",
-  dataset: "production",
-  apiVersion: "2021-10-14",
-  useCdn: false
-});
+// const client = createClient({
+//   projectId: "q21v17fe",
+//   dataset: "production",
+//   apiVersion: "2021-10-14",
+//   useCdn: false
+// });
 
-export async function getServerSideProps(context) {
-  const { blogId } = context.query;
-  const query = `*[_type == "blog" && _id == "${blogId}"]`;
-  const blog = await client.fetch(query);
+// export async function getServerSideProps(context) {
+//   const { blogId } = context.query;
+//   const query = `*[_type == "blog" && _id == "${blogId}"]`;
+//   const blog = await client.fetch(query);
 
-  return {
-    props: {
-      blog: blog[0],
-    },
-  };
-}
+//   return {
+//     props: {
+//       blog: blog[0],
+//     },
+//   };
+// }
