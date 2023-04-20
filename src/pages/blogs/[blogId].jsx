1dde25a5eb923759990510ba223eae4ed5d62cd0
Saurabh Sen <sensaurabh1212@gmail.com>
2023-04-20 09:17:34 +0530

    trying to fix the sanity error

--- src/pages/blogs/[blogId].jsx
+++ src/pages/blogs/[blogId].jsx
@@ -1,34 +1,13 @@
-import { useRouter } from 'next/router';
 import React from 'react'
+import { createClient } from 'next-sanity';
+import { useRouter } from 'next/router';
 import BlogDetailsPage from '../../components/BlogComponents/BlogDetailsPage/index'
-import blog1 from '../../../public/blog/blog1.jpg'
 
-const BlogDetails = () => {
-  const router = useRouter()
-  const { blogId } = router.query;
-
+const BlogDetails = ({blog}) => {
   const [item, setItem] = React.useState({});
   React.useEffect(() => {
-    if (blogId) {
-      const QUERY = encodeURIComponent(`*[_type == "blog" && _id == "${blogId}"]`);
-      fetch(`https://${process.env.NEXT_PUBLIC_PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${process.env.NEXT_PUBLIC_DATASET}?query=${QUERY}`)
-        .then((res) => res.json())
-        .then((data) => setItem(data.result[0]))
-    }
-  }, [blogId]);
-
-  console.log(item);
-
-  // fetch blog details from the blogId and remove the hard coded item
-  //   let item = {
-  //     id: 2,
-  //     imgUrl: blog1,
-  //     title: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, vitae et est ni",
-  //     content: " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima, vitae et est nihil sunt incidunt totam placeat modi quisquam maxime fugiat corrupti, corporis quae ab dicta id ex rem accusamus doloribus ducimus odit nesciunt consequatur similique. Repellat dolores, magni soluta quo, ipsam nulla, obcaecati quae incidunt nobis aperiam doloret ipsa! Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias maiores magni sint reprehenderit ipsam inventore?",
-  //     date: '31/1/2002',
-  //     author: 'vishal dhangar',
-  //     type: 'Invertor'
-  // };
+    setItem(blog);
+  }, []);
 
   return (
     <div className='BlogDetails '>
@@ -38,3 +17,22 @@ const BlogDetails = () => {
 }
 
 export default BlogDetails
+
+const client = createClient({
+  projectId: "q21v17fe",
+  dataset: "production",
+  apiVersion: "2021-10-14",
+  useCdn: false
+});
+
+export async function getServerSideProps(context) {
+  const { blogId } = context.query;
+  const query = `*[_type == "blog" && _id == "${blogId}"]`;
+  const blog = await client.fetch(query);
+
+  return {
+    props: {
+      blog: blog[0],
+    },
+  };
+}
